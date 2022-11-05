a460acf5668857cb6890ea06c6073f02223b5be0
Juan Soares <112213070+JuanSoaress@users.noreply.github.com>
2022-11-04 21:15:39 -0300

    add readme.md

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ function createGroups(Rodada){
     return `
     <header>
 <img src="./assets/logo.svg" alt="Logo da NLW">
-<<h1>${Rodada}</h1> 
 </header>`
 }
 
 
-function createGames(player1, hour, player2){
+function createGames(player1, hour, player2,){
     return `   
 
     <li>
@@ -17,11 +16,14 @@ function createGames(player1, hour, player2){
     </li>`
 }
 let delay = 0.5;
-function createCard(date, day, games,group){
+
+function createCard(date, day, games,group,rodada){
     delay = delay + 0.5;
     return `
     <div class="card" style="animation-delay: ${delay}s" >
     <h3>${group}</h3>
+    <p>${rodada} </p>
+    
     <h2>${date} <span>${day}</span></h2>
     <ul>
         ${games}
@@ -32,15 +34,21 @@ function createCard(date, day, games,group){
 
 document.querySelector("#app").innerHTML = `<header>
 <img src="./assets/logo.svg" alt="Logo da NLW">
-<<h1>Grupo A</h1>, <<h1>Grupo B</h1>
 </header>
 <main id="cards">
-${createCard("20/11","Domingo",  createGames('Catar', '13:00', 'Ecuador'),'1° Rodada')}
-${createCard("21/11", "Segunda",createGames('senegal', '13:00', 'netherlands'), '1° Rodada')}
-${createCard("25/11", "Sexta",createGames('Catar', '10:00', 'senegal'), '2° Rodada')}
-${createCard("25/11", "Sexta",createGames('netherlands', '13:00', 'Ecuador'), '2° Rodada')}
-${createCard("25/11", "Sexta",createGames('netherlands', '12:00', 'Catar'), '3° Rodada')}
-${createCard("25/11", "Sexta",createGames('Ecuador', '12:00', 'senegal'), '3° Rodada')}
+${createCard("20/11","Domingo",  createGames('Catar', '13:00', 'Ecuador'), ('Grupo A'),('1° Rodada') )}
+${createCard("21/11", "Segunda",createGames('senegal', '13:00', 'netherlands'),'Grupo A', '1° Rodada')}
+${createCard("25/11", "Sexta",createGames('Catar', '10:00', 'senegal'),'Grupo A', '2° Rodada')}
+${createCard("25/11", "Sexta",createGames('netherlands', '13:00', 'Ecuador'),'Grupo A','2° Rodada')}
+${createCard("29/11", "Sexta",createGames('netherlands', '12:00', 'Catar'),'Grupo A', '3° Rodada')}
+${createCard("29/11", "Sexta",createGames('Ecuador', '12:00', 'senegal'),'Grupo A', '3° Rodada')}
+
+${createCard("21/11","Domingo",  createGames('england', '10:00','iran'), ('Grupo B'),('1° Rodada') )}
+${createCard("21/11", "Segunda",createGames('senegal', '13:00', 'netherlands'),'Grupo B', '1° Rodada')}
+${createCard("25/11", "Sexta",createGames('Catar', '10:00', 'senegal'),'Grupo B', '2° Rodada')}
+${createCard("25/11", "Sexta",createGames('netherlands', '13:00', 'Ecuador'),'Grupo B','2° Rodada')}
+${createCard("25/11", "Sexta",createGames('netherlands', '12:00', 'Catar'),'Grupo B', '3° Rodada')}
+${createCard("25/11", "Sexta",createGames('Ecuador', '12:00', 'senegal'),'Grupo B', '3° Rodada')}
 </main>
 
 `
